9f7924f9135dc4d0ceba7a1275bdd835abf2c30d
Emilio Gonzalez <emiliofgonzalez7@gmail.com>
2023-01-10 17:32:08 -0600

    🚨 fix linter warning

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,17 +1,16 @@
-export const Footer = () => {
-
-  const currentlyYear = () => {
-    const date = new Date();
-    const year = date.getFullYear();
-    return year || '';
+export const Footer = (): JSX.Element => {
+  const currentlyYear = (): number => {
+    const date = new Date()
+    const year = date.getFullYear()
+    return year
   }
 
   return (
-    <footer className="absolute bottom-0 w-full bg-sz-white py-4 shadow dark:bg-sz-white flex flex-col items-center">
-      <span className="font-bold text-l text-sz-secondary">
+    <footer className='absolute bottom-0 w-full bg-sz-white py-4 shadow dark:bg-sz-white flex flex-col items-center'>
+      <span className='font-bold text-l text-sz-secondary'>
         © {currentlyYear()} Emilio González | #superzombie🧟
       </span>
-      <span className="font-bold text-l text-sz-secondary">
+      <span className='font-bold text-l text-sz-secondary'>
         Made with Love and Next.js
       </span>
     </footer>
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,19 +1,23 @@
-import Head from 'next/head';
+import { FC, ReactNode } from 'react'
+import Head from 'next/head'
 
-import { Header } from 'src/components/header';
-import { Footer } from 'src/components/footer';
+import { Header } from 'src/components/header'
+import { Footer } from 'src/components/footer'
 
-
-export const Layout = ({ children }: any) => {
+export const Layout: FC<Props> = ({ children }): JSX.Element => {
   return (
     <>
       <Head>
         <title>Emilio González | Software Engineer</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
       <Header />
       <>{children}</>
       <Footer />
     </>
   )
+}
+
+interface Props {
+  children: ReactNode
 }
