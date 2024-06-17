a2bdc67181b5e538f9b5efb5ca2c82c757b411a6
Emilio Gonzalez <emiliofgonzalez7@gmail.com>
2024-06-17 22:44:27 +0000

    ⬆️ project upgrade to next@14.2.4

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        background: '#111B21',
+        'dark-green': '#103527',
+        'light-green': '#25D366',
+        'green': '#156038'
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
