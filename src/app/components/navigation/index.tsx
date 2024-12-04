c96394a7686ca46d80bee55a5adf60d7d25586cc
Emilio Gonzalez <emiliofgonzalez7@gmail.com>
2024-12-03 23:53:35 -0600

    🐛 badge effect correction

--- src/app/components/navigation/index.tsx
+++ src/app/components/navigation/index.tsx
@@ -3,10 +3,20 @@ import Links from "@/app/components/navigation/Links"
 export default async function Navigation({ lang }: Lang) {
   console.log('lang', lang)
   return (
-    <nav className="w-full mx-auto px-3 py-2 fixed bottom-0 left-0 right-0 z-10 backdrop-blur-sm md:flex md:justify-center" >
-      <div className="py-1 bg-white bg-opacity-65 border shadow-2xl max-w-screen-xl rounded-3xl md:w-[450px] md:content-center md:px-1 md:py-3 dark:bg-black dark:border-black">
-        <Links lang={lang} />
+    <>
+      <nav className="w-full mx-auto px-3 py-2 fixed bottom-0 left-0 right-0 z-10 md:flex md:justify-center" >
+        <div className="py-1 bg-white bg-opacity-65 border shadow-2xl max-w-screen-xl rounded-3xl md:w-[450px] md:content-center md:px-1 md:py-3 dark:bg-black dark:border-black">
+          <Links lang={lang} />
+        </div>
+      </nav>
+      <div className="gradient-blur h-[75px] md:h-[100px]">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
-    </nav>
+    </>
   )
 }
