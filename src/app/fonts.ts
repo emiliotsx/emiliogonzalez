095bc19f13769461af313a3189c771141ea9ce05
Emilio Gonzalez <emiliofgonzalez7@gmail.com>
2025-01-13 21:23:39 -0600

    🔗 update wedding URLs to Vercel deployments and remove unused font import

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,5 +1,3 @@
-import { Lusitana, Montserrat } from "next/font/google"
+import { Montserrat } from "next/font/google"
 
 export const montserrat = Montserrat({ subsets: ["latin"] })
-
-export const lusitana = Lusitana({ weight: ["400", "700"], subsets: ["latin"] })
